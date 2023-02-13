7015c1c76a029de8f0eff0b02320988df8743bf6
Haruachm <horang_e7@naver.com>
2023-02-13 23:45:35 +0900

    style : Theme Template

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,32 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:flutter_dday_counter/screen/home_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'HiMelody',
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Cookie',
+            fontSize: 70,
+          ),
+          headline2: TextStyle(
+            color: Colors.white,
+            fontSize: 60,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+          ),
+        ),
+      ),
       home: HomeScreen(),
     ),
   );
